5449f2df06b229c79ef21d9c02d7858a76566ca5
thomson209 <doug.thomson@keyin.com>
2021-12-17 14:25:08 -0330

    deleted duplicate code

--- src/main/java/com/keyin/BST.java
+++ src/main/java/com/keyin/BST.java
@@ -123,27 +123,5 @@ class BST {
         preOrder(node.right);
     }
 
-    public static void main(String[] args)  {
-        //create a BST object
-        BST bst = new BST();
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the number of nodes for your tree: ");
-        int n = scanner.nextInt();
-        int[] input = new int[n];
-
-        System.out.println("Please enter the values: ");
-        for (int i =0; i < n; i++) {
-            int currInput = scanner.nextInt();
-            bst.insert(currInput);
-            input[i] = currInput;
-        }
-
-        System.out.println("Pre-order traversal of BST: ");
-        bst.preOrder(BST.root);
-        System.out.println("\nThe root value is: ");
-        System.out.println(BST.root.value);
-        scanner.close();
-
-    }
 }
